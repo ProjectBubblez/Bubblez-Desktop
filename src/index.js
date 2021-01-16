92baf88c1ecd30bc8f54a07401b71f3569f916b9
AFuxy <zakygames701@gmail.com>
2021-01-16 20:28:27 +0000

    v1.0.9-alpha
    Added back and forward buttons

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, Menu } = require('electron');
 const path = require('path');
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
@@ -15,9 +15,9 @@ const createWindow = () => {
     fullscreenable: true,
     autoHideMenuBar: true,
     useContentSize: true,
-    width: 1920,//800,
-    height: 1080,//600,
-    backgroundColor: '#00cc99',
+    width: 1910,//800,
+    height: 1070,//600,
+    backgroundColor: '#282c34',
     transparent: true,
     frame: true,
     icon: path.join(__dirname, 'bubblez.ico')
@@ -26,15 +26,15 @@ const createWindow = () => {
   //const contents = mainWindow.webContents
 
   // and load the index.html of the app.
-  //mainWindow.loadFile(path.join(__dirname, 'index.html'));
-  mainWindow.loadURL('https://bubblez.app/home')
+  mainWindow.loadFile(path.join(__dirname, 'index.html'));
+  //mainWindow.loadURL('https://bubblez.app/home');
 
   // Open the DevTools.
   //mainWindow.webContents.openDevTools();
 
-  // const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
+   //const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
 
-  // Menu.setApplicationMenu(mainMenu);
+   //Menu.setApplicationMenu(mainMenu);
   mainWindow.setIcon(path.join(__dirname, 'bubblez.ico'));
 };
 
@@ -63,21 +63,21 @@ app.on('activate', () => {
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and import them here.
 
-// const mainMenuTemplate = [
-//   {
-//     label: '<--',
-//     // role: 'undo'
-//     //click: function(){
-//       // contents.goBack()
-//       // window.history.back();
-//     //}
-//   },
-//   {
-//     label: '-->',
-//     // role: 'redo'
-//     //click: function(){
-//       // contents.goForward()
-//       // window.history.forward();
-//     //}
-//   }
-//   ];
+ //const mainMenuTemplate = [
+   //{
+     //label: '<--',
+      //role: 'undo',
+      //click: function(){
+        //contents.goBack()
+        //window.history.back();
+     //}
+   //},
+   //{
+     //label: '-->',
+      //role: 'redo',
+      //click: function(){
+        //contents.goForward()
+        //window.history.forward();
+     //}
+   //}
+   //];
